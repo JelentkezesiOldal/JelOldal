d4ca84083e9bf82960b8dbf08c765224931b3245
DeltazorZz <vetka.adi@gmail.com>
2022-12-15 15:08:52 +0100

    Backend alap

---
+++ app/Models/Jelentkezo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jelentkezo extends Model
+{
+    use HasFactory;
+}
