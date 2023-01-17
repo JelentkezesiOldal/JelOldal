653959392277a5dbe78ba35f610e1d304fc6776c
ForGetASD <vitayz60@gmail.com>
2023-01-17 15:56:23 +0100

    FelhasználókView megjelenítés

--- public/js/view/Admin/Felhasznalok/FelhasznalokView.js
+++ public/js/view/Admin/Felhasznalok/FelhasznalokView.js
@@ -1,6 +1,17 @@
 class FelhasznalokView{
-    constructor(){
+    constructor(tomb, szuloElem){
         console.log("FelhasznalokView")
+        szuloElem.html(`<table>
+            <thead>
+                <tr>
+                    <th>Felhaszálónév<th>
+                    <th>E-mail<th>
+                    <th>Telefonszám<th>
+                    <th>Szak<th>
+                    <th>Master<th>
+                </tr>    
+            </thead>
+        </table>`)
     }
 }
 
