8b86469aba5159a6b03708fdb033bdbb9d0d4d75
ForGetASD <vitayz60@gmail.com>
2023-01-20 13:12:55 +0100

    felhasznalokview végleges elrendezése

--- public/js/view/Admin/Felhasznalok/FelhasznalokView.js
+++ public/js/view/Admin/Felhasznalok/FelhasznalokView.js
@@ -24,6 +24,26 @@ class FelhasznalokView{
             </div>
         </div>
         `)
+        szuloElem.append(`<table>
+        <tr>
+            <th>Felhasználónév</th>
+            <th>E-mail</th>
+            <th>Ideiglenes jelszó</th>
+            <th>Jogosultság</th>
+            <th>Szak</th>
+            <th></th>
+        </tr>
+        <tr>
+            <td><input type="text"></td>
+            <td><input type="text"></td>
+            <td><input type="text"></td>
+            <td><input type="text"></td>
+            <td><input type="text"></td>
+            <td><button>Felvesz</button></td>
+        </tr>
+        </table>
+        `)
+
         szuloElem.append(`<table class="table table-dark table-striped">
             <thead>
                 <tr>
