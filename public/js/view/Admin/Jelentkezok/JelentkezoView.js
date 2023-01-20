d78b713abca78e48ffbd6ecdee40881ddc5bbfaf
ForGetASD <vitayz60@gmail.com>
2023-01-20 14:48:07 +0100

    jelentkezőkre kattintva visszakapjuk az id-t

--- public/js/view/Admin/Jelentkezok/JelentkezoView.js
+++ public/js/view/Admin/Jelentkezok/JelentkezoView.js
@@ -3,13 +3,17 @@ class JelentkezoView{
     constructor(elem, szuloElem){
         console.log("jelentkezoView")
         this.#elem = elem
-        szuloElem.append(`<tr>
+        szuloElem.append(`<tr id="J${elem.jelentkezo_id}">
             <td>${elem.OM_azonosito}</td>
             <td>${elem.tanulo_neve}</td>
             <td>${elem.email}</td>
             <td>${elem.telefonszam}</td>
         </tr>`)
+    
+    $("#J"+elem.jelentkezo_id).on("click", () => {
+        console.log("J"+ elem.jelentkezo_id + " katt")
+
+    })
     }
 }
-
 export default JelentkezoView
