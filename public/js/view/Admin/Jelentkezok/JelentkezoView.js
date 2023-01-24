0e8d41750e3369986bff5569877db776c4b6c97c
ForGetASD <vitayz60@gmail.com>
2023-01-24 15:00:54 +0100

    modal kész, összes adat amit ki kell írni ki van

--- public/js/view/Admin/Jelentkezok/JelentkezoView.js
+++ public/js/view/Admin/Jelentkezok/JelentkezoView.js
@@ -14,12 +14,30 @@ class JelentkezoView{
             console.log("J"+ elem.jelentkezo_id + " katt")
             $(".modal").show()
             modal.append(`
-                <p>Ez egy üzenet plusz az OM azonosító: ${elem.OM_azonosito }</p>
+            <div>
+                <p>Tanuló neve: ${elem.tanulo_neve}</p>
+                <p>Születéskori neve: ${elem.szuleteskori_neve}</p>
+                <p>Anyja neve: ${elem.anyja_neve}</p>
+                <p>Születési dátum: ${elem.szuletesi_datum}</p>
+                <p>Születési hely: ${elem.szuletesi_hely}</p>
+                <p>E-mail címe: ${elem.email}</p>
+                <p>Telefonszáma: ${elem.telefonszam}</p>
+                <p>Értesítési cím: ${elem.ertesitesi_cim}</p>
+                <p>Neme: ${elem.neme}</p>
+                <p>OM azonosító: ${elem.OM_azonosito}</p>
+                <p>Személyi igazolvány száma: ${elem.szemelyi_igazolvany_szam}</p>
+                <p>Taj szám: ${elem.taj_szam}</p>
+                <p>Adószám: ${elem.adoszam}</p>
+                <p>Érettségi bizonyítvány száma: ${elem.erettsegi_bizonyitvany_szama}</p>
+                <p>Szakmai bizonyítvány száma: ${elem.szakmai_bizonyitvany_szama}</p>
+                <p>Bankszámlaszám: ${elem.bankszamlaszam}</p>
+                <p>Státusz: ${elem.statusz}</p>
+            </div>
             `)
         })
         $(".close").on("click", () => {
             $(".modal").hide()
-            $(".modal-content p").remove()
+            $(".modal-content div").remove()
         })
     }
 }
