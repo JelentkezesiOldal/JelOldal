b9d87d603ebffc32fe5ab2909617ce184054d4bf
DeltazorZz <vetka.adi@gmail.com>
2023-02-02 11:29:31 +0100

    inditottszak route javitas

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FileController;
+use App\Http\Controllers\InditottSzakController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\JelentkezoController;
 use App\Http\Controllers\SzakController;
