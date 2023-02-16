42a3d06d06f4dc80f138901a2e4a1360371c3cbe
Kata12344 <95749629+Kata12344@users.noreply.github.com>
2023-02-16 20:00:26 +0100

    archivum: keresés funkció (nincs kész)

--- public/js/view/Admin/Archivum/ArchivaltakView.js
+++ public/js/view/Admin/Archivum/ArchivaltakView.js
@@ -12,6 +12,16 @@ class ArchivaltakView {
         this.modalElem = szuloElem.children("div:last-child");
         this.tenylegmodalElem = this.modalElem.children("div:last-child");
         szuloElem.append(`
+        <div class="row float-end">
+            <div class="col input-group">
+                <input type="text" id="keres1" placeholder="Search">
+            </div>
+            <div class="col">
+                <button id="kereses">Keres</button>
+            </div>
+        </div>
+        `)
+        szuloElem.append(`
         <h2 class="cim">Archivált jelentkezések</h2>
         <table class="table table-hover">
             <thead class="thead-light">
@@ -28,6 +38,13 @@ class ArchivaltakView {
         this.tableElem = szuloElem.children("table:last-child");
         this.tbodyElem = this.tableElem.children("tbody");
 
+        this.keresGomb = $('#kereses')
+        this.keresGomb.on('click', ()=>{
+            this.input = $('#keres1').val()
+            console.log(this.input + " lécci új")
+            this.kattintasTrigger("keresJ")
+        })
+
         tomb.forEach((adat) => {
             const adatom = new ArchivaltView(
                 adat,
@@ -35,6 +52,10 @@ class ArchivaltakView {
                 this.tenylegmodalElem
             );
         });
+    }
+    kattintasTrigger(esemenyNeve){
+        const esemeny = new CustomEvent(esemenyNeve, {detail:this.input})
+        window.dispatchEvent(esemeny);
     }
 }
 
