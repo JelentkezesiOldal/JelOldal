73866c323b9a09f4d576dbb54cff2efdee9a22a5
Kata12344 <95749629+Kata12344@users.noreply.github.com>
2023-03-02 19:44:56 +0100

    csv-be mentés sikeres

--- public/js/view/Admin/Jelentkezok/JelentkezokView.js
+++ public/js/view/Admin/Jelentkezok/JelentkezokView.js
@@ -1,8 +1,9 @@
 import JelentkezoView from "./JelentkezoView.js";
 
-class JelentkezokView{
+class JelentkezokView {
     #tomb;
-    constructor(tomb, szuloElem){
+
+    constructor(tomb, szuloElem) {
         this.#tomb = tomb;
         szuloElem.html(`
         <div id="myModal" class="modalSajat">
@@ -10,10 +11,12 @@ class JelentkezokView{
                 <spam class="closeSajat">&times;</spam>    
             </div>
         </div>
-        `)
-        this.modalElem = szuloElem.children("div:last-child")
-        this.tenylegmodalElem = this.modalElem.children("div:last-child")
+        `);
+        this.modalElem = szuloElem.children("div:last-child");
+        this.tenylegmodalElem = this.modalElem.children("div:last-child");
         szuloElem.append(`
+        <div>
+            <button class="row float-left" id="csv">CSV-be ment</button>
         <div class="row float-end">
             <div class="col input-group">
                 <input type="text" id="keres1" placeholder="Search">
@@ -22,7 +25,8 @@ class JelentkezokView{
                 <button id="kereses">Keres</button>
             </div>
         </div>
-        `)
+        </div>
+        `);
         szuloElem.append(`
         <table id="jelTab" class="table table-hover">
             <thead>
@@ -37,67 +41,74 @@ class JelentkezokView{
             <tbody>
             
             </tbody>
-        </table>`)
+        </table>`);
         /** */
         var headers = {
-            // jelentkezo_id: 'jelentkezo_id'.replace(/,/g, ''), // remove commas to avoid errors
+            // jelentkezo_id: "jelentkezo_id".replace(/,/g, ""), // remove commas to avoid errors
             diak_azonosito: "OM azonosito",
             tanulo_neve: "Nev",
             email: "E-mail",
-            telefonszam: "Telefonszam"
+            telefonszam: "Telefonszam",
         };
-        var fileTitle = 'jelentkezok';
-        
+        var fileTitle = "jelentkezok";
+
         var itemsFormatted = [];
         tomb.forEach((item) => {
             itemsFormatted.push({
-                // jelentkezo_id: item.jelentkezo_id.replace(/,/g, ''), // remove commas to avoid errors,
+                // jelentkezo_id: item.jelentkezo_id.replace(/,/g, ""), // remove commas to avoid errors,
                 diak_azonosito: item.diak_azonosito,
                 tanulo_neve: item.tanulo_neve,
                 email: item.email,
-                telefonszam:item.telefonszam
+                telefonszam: item.telefonszam,
             });
         });
         /** */
+        this.csvGomb = $("#csv");
+        this.csvGomb.on("click", () =>{
+            // this.kattintasTrigger("keres");
+            exportCSVFile(headers, itemsFormatted, fileTitle);
+        });
 
-        this.keresGomb = $('#kereses')
-        this.keresGomb.on('click', ()=>{
-            this.input = $('#keres1').val()
-            console.log(this.input + " lécci új")
-            this.kattintasTrigger("keresJ")
-            exportCSVFile(headers, itemsFormatted, fileTitle)
-        })
+        this.keresGomb = $("#kereses");
+        this.keresGomb.on("click", () => {
+            this.input = $("#keres1").val();
+            console.log(this.input + " lécci új");
+            this.kattintasTrigger("keresJ");
+        });
 
-        this.tableElem = szuloElem.children('table:last-child');
-        this.tbodyElem = this.tableElem.children('tbody');
-        tomb.forEach(adat => {
-            const adatom = new JelentkezoView(adat, this.tbodyElem, this.tenylegmodalElem)
+        this.tableElem = szuloElem.children("table:last-child");
+        this.tbodyElem = this.tableElem.children("tbody");
+        tomb.forEach((adat) => {
+            const adatom = new JelentkezoView(
+                adat,
+                this.tbodyElem,
+                this.tenylegmodalElem
+            );
         });
     }
-    kattintasTrigger(esemenyNeve){
-        const esemeny = new CustomEvent(esemenyNeve, {detail:this.input})
+    kattintasTrigger(esemenyNeve) {
+        const esemeny = new CustomEvent(esemenyNeve, { detail: this.input });
         window.dispatchEvent(esemeny);
     }
 }
+
 function ConvertToCSV(objArray) {
-    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-    var str = '';
+    var array = typeof objArray != "object" ? JSON.parse(objArray) : objArray;
+    var str = "";
 
     for (var i = 0; i < array.length; i++) {
-        var line = '';
+        var line = "";
         for (var index in array[i]) {
-            if (line != '') line += ','
+            if (line != "") line += ";";
 
             line += array[i][index];
         }
 
-        str += line + '\r\n';
+        str += line + "\r\n";
     }
-    
-    return str;
-    
-}
 
+    return str;
+}
 
 function exportCSVFile(headers, items, fileTitle) {
     if (headers) {
@@ -109,19 +120,21 @@ function exportCSVFile(headers, items, fileTitle) {
 
     var csv = ConvertToCSV(jsonObject);
 
-    var exportedFilenmae = fileTitle + '.csv' || 'export.csv';
+    var exportedFilenmae = fileTitle + ".csv" || "export.csv";
 
-    var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
-    if (navigator.msSaveBlob) { // IE 10+
+    var blob = new Blob([csv], { type: "text/csv;charset=utf-8;" });
+    if (navigator.msSaveBlob) {
+        // IE 10+
         navigator.msSaveBlob(blob, exportedFilenmae);
     } else {
         var link = document.createElement("a");
-        if (link.download !== undefined) { // feature detection
+        if (link.download !== undefined) {
+            // feature detection
             // Browsers that support HTML5 download attribute
             var url = URL.createObjectURL(blob);
             link.setAttribute("href", url);
             link.setAttribute("download", exportedFilenmae);
-            link.style.visibility = 'hidden';
+            link.style.visibility = "hidden";
             document.body.appendChild(link);
             link.click();
             document.body.removeChild(link);
@@ -129,4 +142,4 @@ function exportCSVFile(headers, items, fileTitle) {
     }
 }
 
-export default JelentkezokView
+export default JelentkezokView;
