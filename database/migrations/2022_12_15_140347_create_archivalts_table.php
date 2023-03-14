6154aae7b36b8e9f2f8429cb4a3ca35eb90338f8
Kata12344 <95749629+Kata12344@users.noreply.github.com>
2023-03-14 12:01:05 +0100

    teszt adatok

--- database/migrations/2022_12_15_140347_create_archivalts_table.php
+++ database/migrations/2022_12_15_140347_create_archivalts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Archivalt;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,6 +20,11 @@ return new class extends Migration
             $table->timestamp('datum')->useCurrent();
             $table->timestamps();
         });
+
+        Archivalt::create(['jelentkezo_id'=> 1,'inditott_id'=>3, 'datum'=>'2022-05-01']);
+        Archivalt::create(['jelentkezo_id'=> 11,'inditott_id'=>4, 'datum'=>'2023-02-21']);
+        Archivalt::create(['jelentkezo_id'=> 7,'inditott_id'=>1, 'datum'=>'2023-05-21']);
+        Archivalt::create(['jelentkezo_id'=> 5,'inditott_id'=>3, 'datum'=>'2023-09-11']);
     }
 
     /**
